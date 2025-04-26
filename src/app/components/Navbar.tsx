738413fa8c48b468262e8aafcf1358b6a650cca5
singhsatyam971 <singhsatyam971@gmail.com>
2025-04-26 20:23:53 +0530

    added animation for menu overlay

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,73 +1,69 @@
 "use client";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import NavLink from "./NavLink";
-import MenuOverlay from "./MenuOverlay";
+import MenuOverlay from "./menu-overlay/MenuOverlay";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
-    { title: "Home", path: "/" },
-    { title: "About", path: "#about" },
-    { title: "Projects", path: "#projects" },
-    { title: "Contact", path: "#contact" },
-  ];
+  { title: "Home", path: "/" },
+  { title: "About", path: "#about" },
+  { title: "Projects", path: "#projects" },
+  { title: "Contact", path: "#contact" },
+];
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
+  const handleNavClick = useCallback(() => {
+    setNavbarOpen((prev) => !prev);
+  }, []);
+
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-gray-900 bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+    <nav className="fixed top-0 left-0 right-0 z-100 bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-8 bg-gray-900">
         <Link
           href="/"
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-            <Image
-              src="globe.svg"
-              alt="Logo"
-              width={100}
-              height={100}
-              className="h-10 w-10 md:h-16 md:w-16"
-            />
+          <Image
+            src="globe.svg"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="h-10 w-10 md:h-16 md:w-16"
+          />
         </Link>
         <div className="block md:hidden">
-          {!navbarOpen ? (
-            <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
-              onClick={() => setNavbarOpen(true)}
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
-              onClick={() => setNavbarOpen(false)}
-            >
+          <button
+            id="nav-toggle"
+            className="flex items-center px-3 py-2 border rounded text-slate-200 border-slate-200 hover:text-white hover:border-white"
+            onClick={handleNavClick}
+          >
+            {navbarOpen ? (
               <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
+            ) : (
+              <Bars3Icon className="h-5 w-5" />
+            )}
+          </button>
         </div>
         <div className="hidden md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
             {navLinks.map((link) => {
               return (
-                <li className="hover:bg-gray-600 hover:border-1 px-3 py-1.5 rounded-full mix-blend-difference" key={link.title}>
-                    <NavLink
-                    title={link.title}
-                    href={link.path}
-                    />
+                <li
+                  className="hover:bg-gray-600 hover:border-1 px-3 py-1.5 rounded-full mix-blend-difference"
+                  key={link.title}
+                >
+                  <NavLink title={link.title} href={link.path} />
                 </li>
               );
             })}
           </ul>
         </div>
       </div>
-      <div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-      </div>
+      <MenuOverlay isVisible={navbarOpen} links={navLinks} />
     </nav>
   );
 };
---
+++ src/app/components/menu-overlay/MenuOverlay.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState, useRef } from "react";
+import NavLink from "../NavLink";
+import "./styles.css";
+
+interface Link {
+  path: string;
+  title: string;
+}
+
+type MenuOverlayProps = {
+  isVisible: boolean;
+  links: Link[];
+};
+
+const MenuOverlay = ({ isVisible, links }: MenuOverlayProps) => {
+  const [show, setShow] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isVisible) {
+      setShow(true);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    const handleAnimationEnd = () => {
+      if (!isVisible) {
+        setShow(false);
+      }
+    };
+
+    const node = menuRef.current;
+    if (node) {
+      node.addEventListener('animationend', handleAnimationEnd);
+    }
+
+    return () => {
+      if (node) {
+        node.removeEventListener('animationend', handleAnimationEnd);
+      }
+    };
+  }, [isVisible]);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div
+      ref={menuRef}
+      className={`bg-gray-900 z-10 ${isVisible ? "menu-overlay--visible" : "menu-overlay--hide"}`}
+    >
+      <ul className="flex flex-col py-4 items-center">
+        {links.map((link, index) => (
+          <li key={index}>
+            <NavLink href={link.path} title={link.title} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MenuOverlay;
